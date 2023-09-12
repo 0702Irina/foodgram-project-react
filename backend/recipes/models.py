f1b2c65a54bb342878f9ad3f810fed321f1e74af
0702Irina <0702irisha@mail.ru>
2023-09-12 19:57:44 +0700

    token

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
+    username = models.CharField(
+        'Логин',
+        unique=True,
+        max_length=200,
+    )
     first_name = models.CharField(
         'Имя',
         max_length=200,
