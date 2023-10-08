f6957c319cd7336b55e2d38d989cac1e850a36ff
0702Irina <0702irisha@mail.ru>
2023-10-08 14:22:46 +0700

    5

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -204,12 +204,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         ).exists()
 
     def get_is_in_shopping_cart(self, obj):
-        request = self.context.get('request')
-        if request is None or request.user.is_anonymous:
-            return False
-        return ActionsForRecipe.objects.filter(
-            user=request.user, recipe_id=obj
-        ).exists()
+        user = self.context['request'].user
+        return user.is_authenticated and obj.in_shopping_cart.contains(user)
 
 
 class IngredientinRecipeCreate(serializers.ModelSerializer):
