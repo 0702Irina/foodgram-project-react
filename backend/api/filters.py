785e93ee8880cf7e02a6160c206c6854f703591d
0702Irina <0702irisha@mail.ru>
2023-10-08 22:37:42 +0700

    78

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -17,9 +17,9 @@ class RecipeFilter(FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    is_farorited = filters.NumberFilter(
-        method='filter_is_favorited'
-    )
+    # is_farorited = filters.NumberFilter(
+    #     method='filter_is_favorited'
+    # )
     is_in_shopping_cart = filters.NumberFilter(
         method='filter_shoppingcart'
     )
@@ -27,16 +27,16 @@ class RecipeFilter(FilterSet):
     class Meta:
         model = Recipe
         fields = (
-            'is_farorited',
+            # 'is_farorited',
             'is_in_shopping_cart',
             'author',
             'tags'
         )
 
-    def filter_is_favorited(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(favorites__user=self.request.user)
-        return queryset
+    # def filter_is_favorited(self, queryset, name, value):
+    #     if value and self.request.user.is_authenticated:
+    #         return queryset.filter(favorites__user=self.request.user)
+    #     return queryset
 
     def filter_shoppingcart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
