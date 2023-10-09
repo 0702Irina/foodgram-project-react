e8215b470c24245d3a1e5cbc8f68eb338a9a3d1c
0702Irina <0702irisha@mail.ru>
2023-10-09 08:05:09 +0700

    5

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -17,30 +17,30 @@ class RecipeFilter(FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    # is_farorited = filters.BooleanFilter(
-    #     method='filter_is_favorited'
-    # )
-    # is_in_shopping_cart = filters.BooleanFilter(
-    #     method='filter_shoppingcart'
-    # )
+    is_farorited = filters.BooleanFilter(
+        method='filter_is_favorited'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_shoppingcart'
+    )
 
     class Meta:
         model = Recipe
         fields = (
-            # 'is_farorited',
-            # 'is_in_shopping_cart',
+            'is_farorited',
+            'is_in_shopping_cart',
             'author',
             'tags'
         )
 
-    # def filter_is_favorited(self, queryset, name, value):
-    #     user = self.request.user
-    #     if value and user.is_authenticated:
-    #         return queryset.filter(is_favorited__user=user)
-    #     return queryset
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorites__user=user)
+        return queryset
 
-    # def filter_is_in_shopping_cart(self, queryset, name, value):
-    #     user = self.request.user
-    #     if value and user.is_authenticated:
-    #         return queryset.filter(sllists__user=user)
-    #     return queryset
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(sllists__user=user)
+        return queryset
