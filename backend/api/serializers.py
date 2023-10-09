052b006850635c5105ab1ac3058fa4ca86ba0f1a
0702Irina <0702irisha@mail.ru>
2023-10-09 23:50:41 +0700

    5

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,11 @@
+import base64
+
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from rest_framework import serializers
+from django.core.files.base import ContentFile
 
 from djoser.serializers import UserSerializer, UserCreateSerializer
-from drf_extra_fields.fields import Base64ImageField
 
 from recipes.models import (
     Shopping_list,
@@ -74,6 +76,17 @@ class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
         fields = '__all__'
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith("data:image"):
+            format, imgstr = data.split(";base64,")
+            ext = format.split("/")[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name="temp." + ext)
+
+        return super().to_internal_value(data)
 
 
 class RecipeShortSerializer(serializers.ModelSerializer):
