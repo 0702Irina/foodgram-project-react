1f864f6ceba99e2d9058a424f467f2ba5cc87d82
0702Irina <0702irisha@mail.ru>
2023-10-10 14:26:13 +0700

    5

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -1,35 +1,24 @@
-from csv import DictReader
-from django.apps import apps
-from django.core.management import BaseCommand
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand, CommandError
+
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Loads data from .csv files to the DB'
+    help = "Импорт ингредиентов из csv файла"
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            'path',
-            nargs='?',
-            type=str,
-            help='Path to the .csv file',
-            default='data/ingredients.csv',
-        )
-        parser.add_argument(
-            'model',
-            nargs='?',
-            type=str,
-            help='Name of the model to data insert',
-            default='Ingredient',
-        )
-
-    def handle(self, *args, **options):
-        path = options.get('path')
-        model = apps.get_model('recipes', options.get('model'))
-
-        with open(path, 'r', encoding='utf-8') as file:
-            for row in DictReader(file):
-                record = model(**row)
-                record.save()
-                self.stdout.write(f'Added to a database: {row}')
-
-            self.stdout.write('Import to a database finished')
+    def handle(self, *args, **kwargs):
+        try:
+            data_path = settings.BASE_DIR
+            with open(
+                f"{data_path}/data/ingredients.csv", "r", encoding="utf-8"
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                Ingredient.objects.bulk_create(
+                    Ingredient(**row) for row in reader
+                )
+            self.stdout.write(self.style.SUCCESS("Все ингредиенты загружены"))
+        except Exception as error:
+            CommandError(str(error))
