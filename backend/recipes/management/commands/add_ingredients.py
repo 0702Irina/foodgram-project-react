53976b69c59523169e36e32fdeafd2a1fb89a6cb
0702Irina <0702irisha@mail.ru>
2023-10-10 22:08:11 +0700

    8

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -9,14 +9,13 @@ class Command(BaseCommand):
     help = 'Импорт ингредиентов из csv файла'
 
     def handle(self, *args, **kwargs):
-        try:
-            with open(
-                '/data/ingredients.csv', 'r', encoding='utf-8'
-            ) as csv_file:
-                reader = csv.DictReader(csv_file)
-                Ingredient.objects.bulk_create(
-                    Ingredient(**row) for row in reader
-                )
-            self.stdout.write(self.style.SUCCESS('Ингредиенты загружены'))
-        except Exception as error:
-            CommandError(str(error))
+        with open(
+            'recipes/data/ingredients.csv', 'r', encoding='utf-8'
+        ) as file:
+            reader = csv.DictReader(file)
+            Ingredient.objects.bulk_create(
+                Ingredient(**row) for row in reader
+            )
+        self.stdout.write(self.style.SUCCESS('Ингредиенты загружены'))
+        return 'OK'
+       
