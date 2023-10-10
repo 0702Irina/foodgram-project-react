c50f29a718d12f2dcbd24a21577ad04c737e7a3f
0702Irina <0702irisha@mail.ru>
2023-10-10 16:20:32 +0700

    4

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -2,11 +2,7 @@ import csv
 
 from recipes.models import Ingredient
 
-MODEL = Ingredient
-
-csv_file_paths = '/app/data/ingredients.cvs'
-
-with open(csv_file_paths, encoding='utf-8', mode='r') as file:
-    csv_reader = csv.DictReader(file)
-    for row in csv_reader:
+with open('/app/data/ingredients.csv', encoding='utf-8', mode='r') as file: 
+    csv_reader = csv.DictReader(file) 
+    for row in csv_reader: 
         Ingredient.objects.get_or_create()
