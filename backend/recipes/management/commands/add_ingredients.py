847ec060c818290442b105f1f858aebf39250c99
0702Irina <0702irisha@mail.ru>
2023-10-10 15:52:19 +0700

    4

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -4,7 +4,7 @@ from recipes.models import Ingredient
 
 MODEL = Ingredient
 
-csv_file_paths = '/backend/data/ingredients.cvs'
+csv_file_paths = '/data/ingredients.cvs'
 
 with open(csv_file_paths, encoding='utf-8', mode='r') as file:
     csv_reader = csv.DictReader(file)
