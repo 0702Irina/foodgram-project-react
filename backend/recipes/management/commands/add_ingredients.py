7c53f385fd14a88143b479cae7a0fa6cd814c300
0702Irina <0702irisha@mail.ru>
2023-10-10 13:15:13 +0700

    5

---
+++ backend/recipes/management/commands/add_ingredients.py
@@ -0,0 +1,35 @@
+from csv import DictReader
+from django.apps import apps
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Loads data from .csv files to the DB'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'path',
+            nargs='?',
+            type=str,
+            help='Path to the .csv file',
+            default='foodgram_static/ingredients.csv',
+        )
+        parser.add_argument(
+            'model',
+            nargs='?',
+            type=str,
+            help='Name of the model to data insert',
+            default='Ingredient',
+        )
+
+    def handle(self, *args, **options):
+        path = options.get('path')
+        model = apps.get_model('recipes', options.get('model'))
+
+        with open(path, 'r', encoding='utf-8') as file:
+            for row in DictReader(file):
+                record = model(**row)
+                record.save()
+                self.stdout.write(f'Added to a database: {row}')
+
+            self.stdout.write('Import to a database finished')
