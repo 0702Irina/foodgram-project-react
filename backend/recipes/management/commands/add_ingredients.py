dc14f90eb4fef0e1c184fecb5fe652d5fb0e8b3a
0702Irina <0702irisha@mail.ru>
2023-10-10 15:36:25 +0700

    4

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -1,24 +1,12 @@
 import csv
 
-from django.conf import settings
-from django.core.management import BaseCommand, CommandError
-
 from recipes.models import Ingredient
 
+MODEL = Ingredient
 
-class Command(BaseCommand):
-    help = "Импорт ингредиентов из csv файла"
+csv_file_paths = '/backend/data/ingredients.cvs'
 
-    def handle(self, *args, **kwargs):
-        try:
-            data_path = settings.BASE_DIR
-            with open(
-                f"{data_path}/data/ingredients.csv", "r", encoding="utf-8"
-            ) as csv_file:
-                reader = csv.DictReader(csv_file)
-                Ingredient.objects.bulk_create(
-                    Ingredient(**row) for row in reader
-                )
-            self.stdout.write(self.style.SUCCESS("Все ингредиенты загружены"))
-        except Exception as error:
-            CommandError(str(error))
+with open(csv_file_paths, encoding='utf-8', mode='r') as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        Ingredient.objects.get_or_create()
