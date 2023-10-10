a39928fbc232b474e506452219027888e69c440f
0702Irina <0702irisha@mail.ru>
2023-10-10 20:31:40 +0700

    5

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -7,18 +7,18 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = "Импорт ингредиентов из csv файла"
+    help = 'Импорт ингредиентов из csv файла'
 
     def handle(self, *args, **kwargs):
         try:
             data_path = settings.BASE_DIR
             with open(
-                f"{data_path}/data/ingredients.csv", "r", encoding="utf-8"
+                f'{data_path}/app/data/ingredients.csv', 'r', encoding='utf-8'
             ) as csv_file:
                 reader = csv.DictReader(csv_file)
                 Ingredient.objects.bulk_create(
                     Ingredient(**row) for row in reader
                 )
-            self.stdout.write(self.style.SUCCESS("Все ингредиенты загружены"))
+            self.stdout.write(self.style.SUCCESS('Ингредиенты загружены'))
         except Exception as error:
             CommandError(str(error))
