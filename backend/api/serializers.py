aab608f72b795fc19e32df69b688ada823c06167
0702Irina <0702irisha@mail.ru>
2023-10-13 19:48:47 +0700

    19/48

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -319,7 +319,7 @@ class RecipeCreateSerializers(serializers.ModelSerializer):
         ingredients_list = []
         for ingredient in ingredients:
             ingredient_id = ingredient['id']
-            if ingredient in ingredients_list:
+            if ingredient_id in ingredients_list:
                 raise serializers.ValidationError(
                     {'ingredients': 'You have already added this ingredient'}
                 )
