06f4c79d0be8fd632d473dba493efe9fe366505a
0702Irina <0702irisha@mail.ru>
2023-10-14 00:04:50 +0700

    00/04

--- backend/backend_food/settings.py
+++ backend/backend_food/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 
 ALLOWED_HOSTS = os.getenv('DJANGO_HOSTS', '127.0.0.1,localhost').split(',')
