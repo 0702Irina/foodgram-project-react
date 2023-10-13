e33769eeff453f81524176b5c75f5010cbd73020
0702Irina <0702irisha@mail.ru>
2023-10-13 15:11:40 +0700

    15/11

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -120,5 +120,6 @@ class UserAdmin(admin.ModelAdmin):
         'email'
     )
     search_fields = ('username', 'email')
+    list_filter = ('email', 'username')
     empty_value_display = '-пусто-'
     verbose_name = 'Пользователь',
